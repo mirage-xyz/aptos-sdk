6e861d33ce955da129625ab54f4aa63efe52ebe1
Kirill Sidorov <38144180+sidorovkirill@users.noreply.github.com>
2022-12-26 18:24:19 +0800

    [MC-5] Add initial library (#1)
    * Add already implemented part of library
    
    * Refactor web helpers to use streams instead of string
    
    * Add forgoten files
    
    * Add token and collections requests, add documentation
    
    * Add events requests and simulation method
    
    * Fixes by comments
    
    * Fix class naming
    
    * Remove redundant async await

---
+++ AptosSDKTest/CoinClientIntegrationTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Numerics;
+using System.Threading.Tasks;
+using Mirage.Aptos.SDK;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace AptosSDKTest
+{
+	[TestFixture]
+	public class CoinClientIntegrationTests
+	{
+		private const string NodeUrl = "https://fullnode.devnet.aptoslabs.com";
+		private const string FaucetUrl = "https://faucet.devnet.aptoslabs.com";
+		private const int TopUpAmount = 100000000;
+
+		private Client _client;
+		private CoinClient _coinClient;
+		private FaucetClient _faucetClient;
+		private Account _aliceAccount;
+		private Account _bobAccount;
+
+		[OneTimeSetUp]
+		public void SetUp()
+		{
+			_client = new Client(NodeUrl);
+			_coinClient = new CoinClient(_client);
+			_faucetClient = new FaucetClient(FaucetUrl, _client);
+			_aliceAccount = new Account();
+			_bobAccount = new Account();
+		}
+
+		[Test, Order(1)]
+		public async Task TopUpAccount()
+		{
+			var account = new Account();
+
+			await _faucetClient.FundAccount(account, TopUpAmount);
+			var balance1 = await _coinClient.GetBalance(account);
+
+			await _faucetClient.FundAccount(account, TopUpAmount);
+			var balance2 = await _coinClient.GetBalance(account);
+
+			Assert.AreEqual(balance1 + TopUpAmount, balance2);
+		}
+
+		[Test, Order(2)]
+		public async Task SignTransaction()
+		{
+			Console.WriteLine("------ Accounts -------");
+			var from = new Account();
+			Console.WriteLine($"Alice: {from.GetAddress()}");
+			var to = new Account();
+			Console.WriteLine($"Bob: {to.GetAddress()}");
+
+			await _faucetClient.FundAccount(from, TopUpAmount);
+			await _faucetClient.FundAccount(to, TopUpAmount);
+
+			Console.WriteLine("------ Balances --------");
+			Console.WriteLine($"Alice's balance: {await _coinClient.GetBalance(from)}");
+			Console.WriteLine($"Bob's balance: {await _coinClient.GetBalance(to)}");
+
+			Console.WriteLine("------ Transaction --------");
+			var pendingTransaction = await _coinClient.Transfer(from, to, 1000);
+			Console.WriteLine("hash = " + pendingTransaction.Hash);
+
+			var awaiter = new TransactionAwaiter(_client);
+			var transaction = await awaiter.WaitForTransactionWithResult(pendingTransaction.Hash);
+
+			Console.WriteLine(JsonConvert.SerializeObject(transaction));
+		}
+	}
+}
---
+++ AptosSDKTest/TokenClientIntegrationTests.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Threading.Tasks;
+using Mirage.Aptos.SDK;
+using Mirage.Aptos.SDK.DTO;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace AptosSDKTest
+{
+	[TestFixture]
+	public class TokenClientIntegrationTests
+	{
+		private const string NodeUrl = "https://fullnode.devnet.aptoslabs.com";
+		private const string FaucetUrl = "https://faucet.devnet.aptoslabs.com";
+		private const int TopUpAmount = 100000000;
+
+		private Client _client;
+		private FaucetClient _faucetClient;
+		private TokenClient _tokenClient;
+		private Account _from;
+		private Account _to;
+		
+		private string _collectionName = "Mirage Aptos SDK";
+		private string _tokenName = "Mirages's first token";
+		private long _tokenPropertyVersion = 0;
+		
+		[OneTimeSetUp]
+		public void SetUp()
+		{
+			_client = new Client(NodeUrl);
+			_faucetClient = new FaucetClient(FaucetUrl, _client);
+			_tokenClient = new TokenClient(_client);
+			
+			_from = new Account();
+			_to = new Account();
+		}
+
+		[Test, Order(1)]
+		public async Task FundAccounts()
+		{
+			await _faucetClient.FundAccount(_from, TopUpAmount);
+			await _faucetClient.FundAccount(_to, TopUpAmount);
+		}
+
+		[Test, Order(2)]
+		public async Task CreateCollection()
+		{
+			var description = "Collection for test Aptos SDK";
+			var uri = "https://mirage.xyz/";
+
+			var transaction = await _tokenClient.CreateCollection(_from, _collectionName, description, uri);
+			Console.WriteLine(transaction.Hash);
+			
+			await RequestMinedTransaction(transaction.Hash);
+
+			var collectionData = await _tokenClient.GetCollectionData(_from.Address, _collectionName);
+			
+			Console.WriteLine(JsonConvert.SerializeObject(collectionData));
+		}
+
+		[Test, Order(3)]
+		public async Task CreateToken()
+		{
+			var tokenDescription = "Mirages's simple token";
+			var transaction = await _tokenClient.CreateToken(
+				_from,
+				_collectionName,
+				_tokenName,
+				tokenDescription,
+				1,
+				"https://mirage.xyz/_next/static/videos/video-desktop-8511e2ee740953e08e74b95f401399f7.webm"
+			);
+			
+			await RequestMinedTransaction(transaction.Hash);
+
+			var tokenData = await _tokenClient.GetTokenData(_from.Address, _collectionName, _tokenName);
+			
+			Console.WriteLine(JsonConvert.SerializeObject(tokenData));
+		}
+
+		[Test, Order(4)]
+		public async Task OfferToken()
+		{
+			var transaction = await _tokenClient.OfferToken(
+				_from,
+				_to.Address,
+				_from.Address,
+				_collectionName,
+				_tokenName,
+				1,
+				_tokenPropertyVersion
+			);
+			
+			Console.WriteLine(transaction.Hash);
+			
+			await RequestMinedTransaction(transaction.Hash);
+			
+			await ShowAccountsBalances();
+		}
+
+		[Test, Order(5)]
+		public async Task ClaimToken()
+		{
+			var transaction = await _tokenClient.ClaimToken(
+				_to,
+				_from.Address,
+				_from.Address,
+				_collectionName,
+				_tokenName,
+				_tokenPropertyVersion
+			);
+			
+			Console.WriteLine(transaction.Hash);
+			await RequestMinedTransaction(transaction.Hash);
+
+			await ShowAccountsBalances();
+		}
+
+		private async Task ShowAccountsBalances()
+		{
+			var fromTokenBalance = await _tokenClient.GetToken(_from.Address, _collectionName, _tokenName, _tokenPropertyVersion);
+			var toTokenBalance = await _tokenClient.GetToken(_to.Address, _collectionName, _tokenName, _tokenPropertyVersion);
+			Console.WriteLine("From balance: " + fromTokenBalance.Amount);
+			Console.WriteLine("To balance: " + toTokenBalance.Amount);
+		}
+		
+		private async Task RequestTransaction(string hash)
+		{
+			var tx = await _client.GetTransactionByHash(hash);
+			// Console.WriteLine(JsonConvert.SerializeObject(tx));
+		}
+		
+		private Task<TypedTransaction> RequestMinedTransaction(string hash)
+		{
+			var awaiter = new TransactionAwaiter(_client);
+			return awaiter.WaitForTransactionWithResult(hash);
+		}
+	}
+}
