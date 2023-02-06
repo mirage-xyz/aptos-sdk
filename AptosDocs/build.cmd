c84ff050aa82744a752f8190022c6ba943f7f787
sidorovk <sidorovk@phxlts.com>
2023-02-06 19:22:01 +0800

    Add docs raw

---
+++ AptosDocs/build.cmd
@@ -0,0 +1,10 @@
+﻿:: 0) Before build docs make sure that all files with in-code documentation was mentioned in config file
+@RD /S /Q ".\_site"
+@RD /S /Q ".\obj"
+:: 1) Generate metadata
+docfx metadata
+:: 2) Create docs
+docfx build
+
+
+
